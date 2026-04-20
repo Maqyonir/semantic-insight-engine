c87ee62c18cddb0565f6c1ba780dfe768dbd8dec
Maqyonir <tauridius@innocent.com>
2026-04-21 05:21:28 +0700

    Feat: add MLEngine class and /upload endpoint

---
+++ app/ml_engine.py
@@ -0,0 +1,35 @@
+from sentence_transformers import SentenceTransformer
+
+
+class MLEngine:
+    """
+    A class to handle text embedding generation using SentenceTransformers.
+
+    Attributes:
+        model (SentenceTransformer): The model used to generate text embeddings.
+    """
+
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2") -> None:
+        """
+        Initializes the MLEngine with a specific pre-trained model.
+
+        Args:
+            model_name (str): The name of the SentenceTransformer model. Defaults to "all-MiniLM-L6-v2".
+        """
+        self.model = SentenceTransformer(  # pyright: ignore[reportUnannotatedClassAttribute]
+            model_name_or_path=model_name
+        )
+
+    def create_embedding(self, text: str) -> list[float]:
+        """
+        Converts a text string into a numerical embedding vector.
+
+        Args:
+            text (str): The input text to process.
+
+        Returns:
+            list[float]: A list of floatting-point number representing the text embedding.
+        """
+        return self.model.encode(  # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
+            text
+        ).tolist()
--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from app.ml_engine import MLEngine
 
 app = FastAPI()
+ml = MLEngine()
 
 
 @app.get(path="/status")
 async def get_status() -> dict[str, str]:
+    """
+    Checks the operational status of the engine.
+
+    Returns:
+        dict[str, str]: A dictionary containing the servcie availability message.
+    """
     return {"status": "The semantic insight engine is ready to work."}
+
+
+@app.post(path="/upload")
+async def upload_document(
+    file: UploadFile = File(
+        default=...
+    ),  # pyright: ignore[reportCallInDefaultInitializer]
+) -> dict[str, str | list[float] | int | None]:
+    """
+    Uploads a text document to generate its vector embedding.
+
+    This endpoint reads the uploaded file, decodes it as UTF-8,
+    and processes the text through the ML engine to create a semantic vector.
+
+    Args:
+        file (UploadFile): The text file to be processed.
+
+    Returns:
+        dict[str, Any]: A summary containing the filename, a preview of the
+        embedding (first 5 elements), and the total vector dimension.
+    """
+    content = await file.read()
+    text = content.decode(encoding="utf-8")
+
+    vector = ml.create_embedding(text)
+
+    return {
+        "filename": file.filename,
+        "vector_preview": vector[:5],
+        "vector_size": len(vector),
+    }
